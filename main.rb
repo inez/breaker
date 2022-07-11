c47792b3cffd6091a4043f5cee689d85f04fec96
Inez Korczyński <inez@squareup.com>
2022-07-11 06:23:05 -0700

    Set work sessions to be 25 minutes by default

--- main.rb
+++ main.rb
@@ -4,7 +4,7 @@ def set_brightness(brightness)
 end
 
 loop do
-  # sleep(60*25)
+  sleep(60*25)
 
   10.times do |index|
     brightness_val = (10-index)/10.to_f
@@ -12,15 +12,14 @@ loop do
     sleep(0.05)
   end
 
-  20.times do
+  40.times do
     set_brightness(0)
-    sleep(1)
+    sleep(0.5)
   end
-  
+
   10.times do |index|
     brightness_val = (1+index)/10.to_f
     set_brightness(brightness_val)
     sleep(0.1)
   end
-  break
 end
